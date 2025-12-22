702a6d8845be3682dd4fbe217ad87f0d5f024083
Merlin Sparrow <anod100002@gmail.com>
2025-12-22 12:23:42 +0400

    ssl

--- src/const/index.ts
+++ src/const/index.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "http://ndiukydev.duckdns.org:10000/api";
-export const MEDIA_BASE_URL = "http://ndiukydev.duckdns.org:10000";
+export const API_BASE_URL = "https://ndiukydev.duckdns.org:20000/api";
+export const MEDIA_BASE_URL = "https://ndiukydev.duckdns.org:20000/";
