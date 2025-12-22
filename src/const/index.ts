39b2d133515934202f2cfac69c27008d038e252c
Merlin Sparrow <anod100002@gmail.com>
2025-12-22 11:54:40 +0400

    Я НЕ ГЕЙ

--- src/const/index.ts
+++ src/const/index.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "http://localhost:3000/api";
-export const MEDIA_BASE_URL = "http://localhost:3000";
+export const API_BASE_URL = "http://ndiukydev.duckdns.org:10000/api";
+export const MEDIA_BASE_URL = "http://ndiukydev.duckdns.org:10000";
