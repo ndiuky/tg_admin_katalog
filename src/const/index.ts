c8d9d8017580e2d731adb62cd37f584feb42f450
Merlin Sparrow <anod100002@gmail.com>
2025-12-23 22:12:11 +0400

    update url НАКОНЕЦ ТО НОРМАЛЬНЫЙ ДОМЕН И ССЛ СЕРТ

--- src/const/index.ts
+++ src/const/index.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "https://ndiukydev.duckdns.org:20000/api";
-export const MEDIA_BASE_URL = "https://ndiukydev.duckdns.org:20000/";
+export const API_BASE_URL = "https://ndiuky.site:20000/api";
+export const MEDIA_BASE_URL = "https://ndiuky.site:20000/";
